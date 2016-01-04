6ec7aebbd0f27e7a78ff435142de51ab1568038b
kenelite <834211743@qq.com>
2016-01-04 21:59:55 +0800

    update pytar

--- pytar.py
+++ pytar.py
@@ -4,6 +4,7 @@
 import tarfile
 import os
 
+# make tar file
 tar = tarfile.open("temp.tar", "w")
 for root, dir, files in os.walk("/tmp/test"):
     for file in files:
@@ -12,9 +13,7 @@ for root, dir, files in os.walk("/tmp/test"):
 tar.close()
 
 
-
-
-
+# make bzip2 file
 bzip2 = tarfile.open("temp.tar.bzip2", "w|bz2")
 for root, dir, files in os.walk("/tmp/test"):
     for file in files:
@@ -22,6 +21,7 @@ for root, dir, files in os.walk("/tmp/test"):
         bzip2.add(fullpath)
 bzip2.close()
 
+# make gipz file
 gzip = tarfile.open("temp.tar.gzip", "w|gz")
 for root, dir, files in os.walk("/tmp/test"):
     for file in files:
@@ -29,6 +29,11 @@ for root, dir, files in os.walk("/tmp/test"):
         gzip.add(fullpath)
 gzip.close()
 
+# list files of tar file
 xtar = tarfile.open("temp.tar.gzip", "r|gz")
 xtar.list()
+xtar.name
+xtar.getnames()
+xtar.members
+#xtar.extractall()
 xtar.close()
